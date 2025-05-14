e2a9c8b895fd1be697194fe6793433b3f79635c7
Mark Walker <mwalker@tamarkdesigns.com>
2025-05-14 14:46:49 -0600

    Initial commit: Task Manager PoC with LangGraph.

--- my_agent/utils/nodes.py
+++ my_agent/utils/nodes.py
@@ -8,7 +8,7 @@ from langgraph.prebuilt import ToolNode
 @lru_cache(maxsize=4)
 def _get_model(model_name: str):
     if model_name == "openai":
-        model = ChatOpenAI(temperature=0, model_name="gpt-4o")
+        model = ChatOpenAI(temperature=0, model_name="gpt-4")
     elif model_name == "anthropic":
         model =  ChatAnthropic(temperature=0, model_name="claude-3-sonnet-20240229")
     else:
@@ -19,7 +19,7 @@ def _get_model(model_name: str):
 
 # Define the function that determines whether to continue or not
 def should_continue(state):
-    messages = state["messages"]
+    messages = state.messages
     last_message = messages[-1]
     # If there are no tool calls, then we finish
     if not last_message.tool_calls:
@@ -33,9 +33,9 @@ system_prompt = """Be a helpful assistant"""
 
 # Define the function that calls the model
 def call_model(state, config):
-    messages = state["messages"]
+    messages = state.messages
     messages = [{"role": "system", "content": system_prompt}] + messages
-    model_name = config.get('configurable', {}).get("model_name", "anthropic")
+    model_name = config.get('configurable', {}).get("model_name", "openai")
     model = _get_model(model_name)
     response = model.invoke(messages)
     # We return a list, because this will get added to the existing list
--- my_agent/utils/state.py
+++ my_agent/utils/state.py
@@ -1,6 +1,7 @@
 from langgraph.graph import add_messages
 from langchain_core.messages import BaseMessage
-from typing import TypedDict, Annotated, Sequence
+from typing import Annotated, Sequence
+from pydantic import BaseModel
 
-class AgentState(TypedDict):
+class AgentState(BaseModel):
     messages: Annotated[Sequence[BaseMessage], add_messages]
